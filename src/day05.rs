c237239a90cf104fa2f99b8f2cb9331b946b4d34
Andreas Neuhaus <zargony@zargony.com>
2016-12-05 10:47:10 +0100

    Day 5 part two

--- src/day05.rs
+++ src/day05.rs
@@ -1,48 +1,84 @@
 extern crate md5;
 
-/// A password finder uses a brute force approach to find
-/// the next character of a "password" for a given door id.
-pub struct PasswordFinder<'a> {
-    door_id: &'a str,
+use std::fmt::Write as fmtWrite;
+use std::io::Write as ioWrite;
+
+/// A hash finder uses a brute force approach to find a hash
+/// with a starting zeroes for a given prefix
+pub struct HashFinder {
     pad: u32,
     ctx: md5::Context,
 }
 
-impl<'a> PasswordFinder<'a> {
-    /// Create new password finder for the given prefix
-    fn new(door_id: &str) -> PasswordFinder {
+impl HashFinder {
+    /// Create new hash finder for the given prefix
+    fn new(prefix: &str) -> HashFinder {
         let mut ctx = md5::Context::new();
-        ctx.consume(door_id.as_bytes());
-        PasswordFinder {
-            door_id: door_id,
+        ctx.consume(prefix.as_bytes());
+        HashFinder {
             pad: 0,
             ctx: ctx,
         }
     }
 }
 
-impl<'a> Iterator for PasswordFinder<'a> {
-    type Item = char;
+impl Iterator for HashFinder {
+    type Item = (u8, u8);
 
-    fn next(&mut self) -> Option<char> {
+    /// Finds the next MD5 hash with 5 leading nibbles by appending an
+    /// ever increasing number to the prefix. Yields the next two
+    /// nibbles
+    fn next(&mut self) -> Option<(u8, u8)> {
         loop {
-            let s = format!("{}", self.pad);
             let mut ctx = self.ctx.clone();
-            ctx.consume(s.as_bytes());
+            // ctx.consume(format!("{}", self.pad).as_bytes());
+            ctx.write_fmt(format_args!("{}", self.pad)).unwrap();
             let hash = ctx.compute();
             self.pad += 1;
             if hash[0..2] == [0; 2] && hash[2] & 0xf0 == 0 {
-                let res = format!("{:1x}", hash[2] & 0x0f);
-                return res.chars().nth(0);
+                return Some((hash[2] & 0x0f, hash[3] >> 4));
             }
         }
     }
 }
 
+/// Helper function for displaying a nibble
+fn nibble2char(n: u8) -> char {
+    match n {
+        0...9 => ('0' as u8 + n) as char,
+        10...15 => ('a' as u8 + n - 10) as char,
+        _ => panic!("nibble must be in range 0..15"),
+    }
+}
+
+/// Find (simple) password for the given door id
+fn find_password(door_id: &str, len: usize) -> String {
+    let finder = HashFinder::new(door_id);
+    finder.take(len).fold(String::with_capacity(len), |mut s, (b, _)| {
+        s.write_char(nibble2char(b)).unwrap();
+        s
+    })
+}
+
+/// Find enhanced password for the given door id
+fn find_enhanced_password(door_id: &str, len: usize) -> String {
+    let mut password: Vec<u8> = vec![0; len];
+    for (pos, b) in HashFinder::new(door_id) {
+        let pos = pos as usize;
+        if pos < password.len() && password[pos] == 0 {
+            password[pos] = nibble2char(b) as u8;
+        }
+        if password.iter().all(|b| *b != 0) { break; }
+    }
+    String::from_utf8(password).unwrap()
+}
+
 fn main() {
-    let finder = PasswordFinder::new("wtnhxymk");
-    let password: String = finder.take(8).collect();
+    let input = "wtnhxymk";
+    let password = find_password(input, 8);
     println!("Password: {}", password);
+    let password = find_enhanced_password(input, 8);
+    println!("Enhanced password: {}", password);
 }
 
 #[cfg(test)]
@@ -51,11 +87,12 @@ mod tests {
 
     #[test]
     fn finding_next_password_character() {
-        let mut finder = PasswordFinder::new("abc");
+        let mut finder = HashFinder::new("abc");
         finder.pad = 3200000;   // speed up a bit for testing
-        assert_eq!(finder.next(), Some('1'));
+        assert_eq!(finder.next(), Some((0x1, 0x5)));
         finder.pad = 5000000;   // speed up a bit for testing
-        assert_eq!(finder.next(), Some('8'));
-        assert_eq!(finder.next(), Some('f'));
+        assert_eq!(finder.next(), Some((0x8, 0xf)));
+        assert_eq!(finder.next(), Some((0xf, 0x9)));
+        assert_eq!(finder.next(), Some((0x4, 0xe)));
     }
 }
