2d121682bd35d97f5e63602ebf34edbec07e71c4
Andreas Neuhaus <zargony@zargony.com>
2016-12-07 10:14:12 +0100

    Day 7 part two

--- src/day07.rs
+++ src/day07.rs
@@ -10,9 +10,18 @@ pub fn count_tls_ips(input: &str) -> usize {
     }).count()
 }
 
+pub fn count_ssl_ips(input: &str) -> usize {
+    let re1 = Regex::new("(?:^|\\])[a-z]*([a-z])(?!\\1)([a-z])\\1.*\\[[a-z]*\\2\\1\\2").unwrap();
+    let re2 = Regex::new("\\[[a-z]*([a-z])(?!\\1)([a-z])\\1.*\\][a-z]*\\2\\1\\2").unwrap();
+    input.lines().filter(|ip| {
+        re1.find(ip).is_some() || re2.find(ip).is_some()
+    }).count()
+}
+
 fn main() {
     let input = include_str!("day07.txt");
     println!("Number of IPs with TLS support: {}", count_tls_ips(input));
+    println!("Number of IPs with SSL support: {}", count_ssl_ips(input));
 }
 
 #[cfg(test)]
@@ -25,5 +34,13 @@ mod tests {
         assert_eq!(count_tls_ips("abcd[bddb]xyyx"), 0);
         assert_eq!(count_tls_ips("aaaa[qwer]tyui"), 0);
         assert_eq!(count_tls_ips("ioxxoj[asdfgh]zxcvbn"), 1);
+    }
+
+    #[test]
+    fn ssl_support_detection() {
+        assert_eq!(count_ssl_ips("aba[bab]xyz"), 1);
+        assert_eq!(count_ssl_ips("xyx[xyx]xyx"), 0);
+        assert_eq!(count_ssl_ips("aaa[kek]eke"), 1);
+        assert_eq!(count_ssl_ips("zazbz[bzb]cdb"), 1);
     }
 }
